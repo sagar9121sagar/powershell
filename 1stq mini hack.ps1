55fb886026542ead7ff5835dabf5aa8c22ca5e6b
sagar9121sagar <100359338+sagar9121sagar@users.noreply.github.com>
2022-04-07 12:31:35 +0530

    Add files via upload

---
+++ 1stq mini hack.ps1
@@ -0,0 +1,3 @@
+﻿ write-Host("sagar")
+ Get-Service | Select-Object  Name, Status,DependentServices,RequiredServices | Export-Csv C:\Users\vadnala.sagar\sagar\qw.csv  
+ import-csv C:\Users\vadnala.sagar\sagar\qwe.csv 
