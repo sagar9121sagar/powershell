4dce32bbbf861d0d3bc0c920d5a58bf8d905942a
sagar9121sagar <100359338+sagar9121sagar@users.noreply.github.com>
2022-04-07 06:57:34 +0000

    Add files via upload

---
+++ email powershell.ps1
@@ -0,0 +1,11 @@
+﻿$Processor = Get-WmiObject -Class win32_processor
+$Processor
+
+$mymail="sagarvadnal77@outlook.com"
+$smtp = "smtp.outlook.com"
+$to = "mohit.my844@outlook.com"
+$sub="power shell task"
+$body= get-content -Path
+$creds = (Get-Credential -Credential "$myemail")
+start-sleep 2
+Send-MailMessage -To $to -From $mymail -Subject $sub -Body $body -SmtpServer $smtp -Credential $creds -UseSsl -Port 578 -DeliveryNotificationOption OnSuccess
---
+++ event logs.ps1
@@ -0,0 +1,7 @@
+﻿Write-Host ("sagar") 
+Get-EventLog -LogName System 
+Get-EventLog -LogName Security 
+Get-EventLog -LogName Application 
+Get-EventLog -LogName Application | Where-Object {$_.InstanceId -eq '4624','4672','9027'
+} 
+Write-Host ("output")
